c00eb00f5fe790aec215f404399d48b35484e688
WifekRaissi <wifek.raissi@esprit.tn>
2018-02-19 19:52:44 +0100

    version2

--- src/main/kotlin/com/kotlin/springkotlinwithservices/controllers/UserController.kt
+++ src/main/kotlin/com/kotlin/springkotlinwithservices/controllers/UserController.kt
@@ -15,6 +15,22 @@ class UserController(){
     @GetMapping("/hello")
     fun helloUser(@RequestParam(value = "name") name: String)=
         userService.HelloUser("$name")
+
+@PostMapping("/add")
+fun addUser(@RequestBody user:User)=
+        userService.addUser(user)
+
+    @PutMapping("/update")
+    fun updateUser(@RequestParam(value = "name") surname: String,@RequestBody user:User) =
+        userService.updateUser(surname, user)
+
+    @DeleteMapping("/delete")
+    fun deleteUser(@RequestParam(value = "name") name: String,@RequestBody user:User) =
+            userService.deleteUser(name,user)
     }
+
+
+
+
 
 
--- src/main/kotlin/com/kotlin/springkotlinwithservices/services/UserServicesImpl.kt
+++ src/main/kotlin/com/kotlin/springkotlinwithservices/services/UserServicesImpl.kt
@@ -12,18 +12,34 @@ class UserServiceImpl: UserService {
 
 
 
-  /*  override fun HelloUser(name: String): String {
-       return ("Hello, $name")
-    }
-*/
 
-    override fun HelloUser(name: String):User {
-        return User(counter.incrementAndGet(),"name:$name",surname = "",birthDate = Date())
+    override fun HelloUser(name:String):User {
 
-
+        return User(counter.incrementAndGet(), "hello,$name", "", birthDate=Date())
     }
 
 
+    override fun addUser(user: User): User {
+        return User(counter.incrementAndGet(),user.name,user.surname,user.birthDate)
+
+
+
+    }
+
+    override fun updateUser(surname: String,user:User): User {
+
+        return User(counter.incrementAndGet(),user.name,surname,user.birthDate)
+
+
+
+
+    }
+    override fun deleteUser(name: String, user: User): String {
+        if (user.name == name) {
+            return  name
+        }
+        return ("$name,deleted")
+    }
 
 
 }
